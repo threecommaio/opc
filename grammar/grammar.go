c594995492f78a8352f6ed017d7283f1982a0759
Chris Goffinet <chris@threecomma.io>
2022-03-05 23:03:44 -0500

    feat: add slack to webhooks (#39)
    GitOrigin-RevId: 9407405eb436f8d452968abd0e4753289f474feb

---
+++ grammar/grammar.go
@@ -0,0 +1,14 @@
+package grammar
+
+import "unicode"
+
+// TitleCase returns a title-cased string
+func TitleCase(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	r := []rune(s)
+	r[0] = unicode.ToUpper(r[0])
+	return string(r)
+}
--- web/signature.go
+++ web/signature.go
@@ -1,12 +1,14 @@
 package web
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/v43/github"
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 )
 
 const (
@@ -28,6 +30,26 @@ type Signature struct {
 
 // SlackValidator validates slack webhook signature
 func SlackValidator(c *gin.Context, data []byte, secret string) error {
+	// handle parsing slack events including challenge requests
+	event, err := slackevents.ParseEvent(data, slackevents.OptionNoVerifyToken())
+	if err != nil {
+		return err
+	}
+	// challenge request for first time setting up the webhook in slack app
+	if event.Type == slackevents.URLVerification {
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal(data, &r)
+		if err != nil {
+			return fmt.Errorf("error parsing challenge response: %s", err)
+		}
+		c.Writer.Header().Set("Content-Type", "text")
+		_, err = c.Writer.Write([]byte(r.Challenge))
+		if err != nil {
+			return fmt.Errorf("error writing challenge response: %s", err)
+		}
+		return nil
+	}
+
 	sv, err := slack.NewSecretsVerifier(c.Request.Header, secret)
 	if err != nil {
 		return fmt.Errorf("%w: %s", ErrVerifyingSlack, err)
